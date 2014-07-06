0466d09fb4241494521dbb2a254993c27421b59b
hanbyul_here <hanbyul@nyu.edu>
2014-07-06 19:16:57 -0400

    api key changed

--- js/foundation/foundation.orbit.js
+++ js/foundation/foundation.orbit.js
@@ -32,7 +32,7 @@
       if (settings.slide_number) {
         if(index == 0) number_container.find('span:first').text(" 라오스 현지의 커피 농장 ");
         if(index == 1) number_container.find('span:first').text(" 고양시 매장 ");
-        if(index == 2) number_container.find('span:first').text(" 라오스의 마스코트 코끼리 ");
+        if(index == 2) number_container.find('span:first').text(" 라오스의 마스코트 코끼 ");
         
       }
       if (settings.bullets) {
